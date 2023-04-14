8ca7ba56977968fa82b4ade0fbb87e4174e26ffe
George Daskalakis <george.dask97@gmail.com>
2023-04-14 19:48:47 +0300

    style: add card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+	import sidebar from '$lib/assets/images/bg-sidebar-desktop.svg';
+</script>
+
 <h2 class="text-primary">Text Inputs</h2>
 
 <div class="form-control">
@@ -36,3 +40,35 @@
 
 <button class="btn">Next Step</button>
 <button class="btn btn-ghost">Go back</button>
+
+<h2 class="text-primary">Cards</h2>
+
+<div class="card">
+	<img src={sidebar} class="card-image" alt="background" />
+	<div class="card-body">
+		<h2 class="card-title">Personal info</h2>
+		<h3 class="card-subtitle">Please provide your name</h3>
+
+		<div class="card-content">
+			<div class="form-control">
+				<label for="name" class="label">Name</label>
+				<input placeholder="e.g. Stephen King" class="input" id="name" />
+			</div>
+
+			<div class="form-control">
+				<label for="email" class="label">Email Address</label>
+				<input placeholder="e.g. stepheking@lorem.com" class="input" id="email" />
+			</div>
+
+			<div class="form-control">
+				<label for="phone" class="label">Phone Number</label>
+				<input placeholder="e.g. +1 234 567 890" class="input" id="phone" />
+			</div>
+		</div>
+
+		<div class="card-actions">
+			<button class="btn btn-ghost">Go back</button>
+			<button class="btn">Next Step</button>
+		</div>
+	</div>
+</div>
